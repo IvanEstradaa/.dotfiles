fe6d4a5a540388a463775155fe42a14f914a7138
IvanEstradaa <ivanestrada@live.com>
2025-04-26 22:56:23 -0600

    Updated Chrome and MacOs setup

--- chrome/setup.sh
+++ chrome/setup.sh
@@ -9,3 +9,20 @@ PROFILE_PATH="$HOME/Library/Application Support/librewolf/Profiles/u0qnsa0l.defa
 
 # Symlink
 ln -s $PWD/chrome/ $PROFILE_PATH
+
+# Set the default browser to LibreWolf 
+open -a "LibreWolf" --new --args -silent -nosplash -setDefaultBrowser
+# Chromium-based browsers: open -a "Google Chrome" --new --args --make-default-browser
+
+# Run the following osascript script to accept the default browser prompt:
+# osascript <<EOF
+# tell application "System Events"
+#     tell application process "CoreServicesUIAgent"
+#         tell window 1
+#             tell (first button whose name starts with "Us")
+#                 perform action "AXPress"
+#             end tell
+#         end tell
+#     end tell
+# end tell
+# EOF
